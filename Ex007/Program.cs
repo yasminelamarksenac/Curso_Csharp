63fe15daee2135b9b9fb87e70de9d46e01c46319
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-16 21:06:19 -0300

    Atualização do exercício

---
+++ Ex007/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 50;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "Aprovado";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+
+            Console.WriteLine("O aluno foi {0}.", resultado);
+
+        }
+    }
+}
+
+
