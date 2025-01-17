cf0449aab461943e1a93786a42bfd6aecb7109bd
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-16 22:09:10 -0300

    Adicionando notas bimestrais

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -6,8 +6,22 @@ namespace Ex007
     {
         static void Main(string[] args)
         {
-            double nota_final = 90;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
+
+            Console.Write("Digite a primeira nota do aluno: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota do aluno: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a  terceira nota do aluno: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quarta nota do aluno: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
 
             if (nota_final >= 60)
             {
