d82856c3f449e2420edeb17c04316aaa6ab34ca6
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-21 20:11:46 -0300

    Adicionando os tipos de triângulos

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -22,6 +22,18 @@
             if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
             {
                 Console.WriteLine("Os valores informados formam um triângulo.");
+                if ((ladoA == ladoB) && (ladoB == ladoC))
+                {
+                    Console.WriteLine("E ele é um triângulo Equilátero");
+                }
+                else if ((ladoA == ladoB) || (ladoA == ladoC))
+                {
+                    Console.Write("E ele é um Triângulo Isóceles .");
+                }
+                else
+                {
+                    Console.WriteLine("E ele é um Triângulo Escaleno");
+                }
             }
             else
             {
