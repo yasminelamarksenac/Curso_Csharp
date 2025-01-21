6b343bbfcab9a61dd1daf4773a2727cd09b5bf6b
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-21 19:32:38 -0300

    Exercício de teste de triângulo

---
+++ Ex009/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("Programa para Testar Triângulos");
+            Console.WriteLine("==================================");
+
+            Console.Write("Digite o valor do lado A:");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo. ");
+                
+            }
+        }
+    }
+}
