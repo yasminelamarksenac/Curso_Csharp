23a443d3d73772f9b69a82b957d284121de63edc
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-21 22:10:46 -0300

    Exercicio 0010 empréstimo de casa

---
+++ Ex0010/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex0010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o salário do funcionario: R$ ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            double aumento;
+
+            if (salario > 2500)
+            {
+                aumento = salario * 0.05;
+            }
+            else if (salario > 1250)
+            {
+                aumento = salario * 0.10;
+            }
+            else
+            {
+                aumento = salario * 0.15;
+            }
+            double novoSalario = salario + aumento;
+            Console.WriteLine("Aumento: R$ " + aumento.ToString("F2"));
+            Console.WriteLine("novoSalario: R$ " + novoSalario.ToString("F2"));
+
+        }
+    }
+}
---
+++ Ex0011/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o valor da casa: R$");
+            double valorCasa = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o salário do comprador? ");
+            double salario = int.Parse(Console.ReadLine());
+
+            Console.Write("Em quantos anos deseja pagar");
+            int anos = int.Parse(Console.ReadLine());
+
+            int meses = anos * 12;
+            double prestacao = valorCasa / meses;
+
+            double limite = salario * 0.3;
+
+            Console.WriteLine("n--- Resultado ---");
+            if (prestacao <= limite)
+            {
+                Console.WriteLine("Empréstimo aprovado!");
+                Console.WriteLine($"Prestação mensal de R$ {prestacao:F2} excede 30% do salário (R$ {limite:F2}.");
+            }
+        }
+    }
+
+}
