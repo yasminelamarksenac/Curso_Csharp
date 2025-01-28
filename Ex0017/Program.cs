469ee08cfb0d222a0fe839b7f63cb31b4634425e
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-28 19:46:28 -0300

    Exercicio Senha

---
+++ Ex0017/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex0017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+
+            while (num < 5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }
+
+            do
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }
+            while (num < 5);
+        }
+    }
+}
---
+++ Senha/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123"; // Senha correta
+            string senhaDigitada; // senha digitada pelo usuário
+            int tentativas = 0; // Contador de tentativas
+             
+
+
+            // Loop para solicitar a senha ao usuário
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++; // Incrementa o contador de tentativas
+
+                // Verifica se a senha digitada é diferente da senha correta
+                if (senhaDigitada != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!");
+                    Console.WriteLine("Você ainda tem " + (3 - tentativas) + " tentativa(s)");
+                    Console.ReadKey();
+                }
+                else if (senhaDigitada != senha && tentativas == 3)
+                {
+                    Console.WriteLine("Senha incorreta! Seu Acesso esta bloquedo!");
+                    return;
+                }
+            } while (senhaDigitada != senha);
+
+            Console.Clear();
+            Console.WriteLine("Senha Correta! - Tentativas: " + tentativas);
+        }
+    }
+}
