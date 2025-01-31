3b7015668e357000d7c581030879150c6876bb16
Yasmine Lamark <yasmine.nlamark@senacsp.edu.br>
2025-01-30 21:28:42 -0300

    Área do triângulo

---
+++ Frutas/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Frutas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "melancia", "manga", "pêra", "uva", "abacaxi", "carambola", "jabuticaba", "morango", "banana", "limão"};
+            double[] preco = { 20, 6, 99, 9, 99, 12, 90, 10, 99, 7, 99, 19, 90, 15, 00, 10, 90, 5, 99 };
+        }
+    }
+}
